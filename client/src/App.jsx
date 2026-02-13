ec652571ad2334515305bf83bb55c93ef4b6959c
StephenNg25 <stephen.n180325@gmail.com>
2026-02-13 16:48:38 -0500

    Adding home page with nav bar components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Login from './Components/Auth/Login'
-import Register from './Components/Auth/Register'
-import Home from './Components/Store/Home'
+import Login from './Login'
+import Register from './Register'
+import Home from './Home'
 
 function App() {
   return (
@@ -10,6 +10,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/checkout" element={<div style={{ padding: 24, color: "white" }}>Checkout page (WIP)</div>} />
+        <Route path="/product/:id" element={<div style={{ padding: 24, color: "white" }}>Product detail (WIP)</div>} />
       </Routes>
     </Router>
   )
